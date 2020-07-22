1a1d2c083b30f367adfcf35e8dcfe52825c78e5a
unknown <bellaqamendim@gmail.com>
2020-07-22 11:44:42 +0200

    changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mendim
                 </div>
 
                 <div class="links">
